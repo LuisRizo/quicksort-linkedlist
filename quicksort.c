7b0bd921d8003b332283d351df222507451f66a4
LuisRizo <personal.luisrizo@gmail.com>
2018-09-23 23:50:46 -0400

    Move to using fprintf for debugging

--- quicksort.c
+++ quicksort.c
@@ -13,28 +13,40 @@ void printAddress(struct listnode * p) { printf("%p\n", p); }
 
 int randomindex(int k) { return (rand() % k); }
 
-void debugCrash(int n) { //printf("Crash Spot #%d", n); 
+void debugCrash(FILE * fptr, int n) { //fprintf(fptr, "Crash Spot #%d", n); 
 }
 
-void swapNodes(struct listnode ** list, int i1, int i2);
+void quit(FILE * fptr) {
+	fclose(fptr);
+	exit(0);
+}
+
+void swapNodes(FILE * fptr, struct listnode ** list, int i1, int i2);
 struct listnode * getNode(struct listnode * a, int idx);
 long getLengthOfListnode(struct listnode * a);
-struct listnode * sort(struct listnode * a);
+struct listnode * sort(FILE * fptr, struct listnode * a);
 void printListnode(struct listnode * list);
 
 int main(void)
 {  
+	FILE *fptr;
 	// char buffer[65536];
    long i, max_reps = 500;
    double time_spent;
    clock_t begin, end;
    struct listnode *node, *space;
+	fptr = fopen("quicksort.log", "w");
+	if (fptr == NULL)
+    {
+        printf("File does not exists \n");
+        exit(1);
+    }
 	// setvbuf(stdout, buffer, _IOFBF, sizeof(buffer));
 
    space =  (struct listnode *) malloc(max_reps * sizeof(struct listnode));
    while(1) {
 		begin = clock();
-		printf("Begin: %lu", begin);
+		// fprintf(fptr, "Begin: %lu", begin);
    		srand(2012);
 		for( i=0; i< max_reps; i++ )
 		{  (space + i)->key = 2*((17*i)%max_reps);
@@ -43,29 +55,27 @@ int main(void)
 		(space+max_reps - 1)->next = NULL;
 		node = space;
 		printf("\n prepared list, now starting sort\n");
-		node = sort(node);
+		node = sort(fptr, node);
 		printf("\n checking sorted list\n");
 		for( i=0; i < max_reps; i++)
 		{  if( node == NULL )
-			{  printf("List ended early | %ld\n", i); exit(0);
+			{  printf("List ended early | %ld\n", i); quit(fptr);
 			}
 			if( node->key != 2*i )
-			{  printf("Node contains wrong value, %ld\n", i); exit(0);
+			{  printf("Node contains wrong value, %ld\n", i); quit(fptr);
 			}
 			node = node->next;
 		}
 		end = clock();
 		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 		printf("Sort successful in %f seconds\n", time_spent);
-		exit(0);
    }
-   exit(0);
+   quit(fptr);
 }
 
 void printListnode(struct listnode * list) {
 	int i = 0, length = getLengthOfListnode(list);
 	for(i = 0; i < length; i++) {
-		debugCrash(11);
 		printf("%ld,", getNode(list, i)->key);
 	}
 	printf("\n");
@@ -99,7 +109,7 @@ long getLengthOfListnode(struct listnode * a) {
 	return length;
 }
 
-void swapNodes(struct listnode ** list, int i1, int i2) {
+void swapNodes(FILE * fptr, struct listnode ** list, int i1, int i2) {
 	struct listnode * node, * val, * prevNode, * prevVal, * tmp;
 	if (i1 == i2) return;
 	node = getNode(*list, i1);
@@ -121,48 +131,50 @@ void swapNodes(struct listnode ** list, int i1, int i2) {
 	}else
 		*list = node;
 
-	debugCrash(4);
+	debugCrash(fptr, 4);
 	tmp = val->next;
-	debugCrash(5);
+	debugCrash(fptr, 5);
 	val->next = node->next;
-	debugCrash(6);
+	debugCrash(fptr, 6);
 	node->next = tmp;
 }
 
-struct listnode * sort(struct listnode * node){
+struct listnode * sort(FILE * fptr, struct listnode * node){
 
 	int i, j1, j2;
 	long length;
 	struct listnode * pivot;
+
+	// freopen("quicksort.log", "w", fptr);
 
 	length = getLengthOfListnode(node);
 	if (length > 1){
 		i = randomindex(length);
 		pivot = getNode(node, i);
 
-		swapNodes(&node, i, length - 1);
+		swapNodes(fptr, &node, i, length - 1);
 
 		j1 = 0; j2 = length - 1;
 		while(j1 < j2) {
-			debugCrash(7);
+			debugCrash(fptr, 7);
 			for(; getNode(node, j1)->key < pivot->key; j1++);
-			debugCrash(8);
+			debugCrash(fptr, 8);
 			for(; getNode(node, j2)->key >= pivot->key && j2 > j1; j2--);
 			
 			if (j1 != j2) {
-				swapNodes(&node, j1, j2);
+				swapNodes(fptr, &node, j1, j2);
 			}
 		}
-		swapNodes(&node, length - 1, j1);
+		swapNodes(fptr, &node, length - 1, j1);
 
 		if (j1 != 0) {
-			debugCrash(9);
+			debugCrash(fptr, 9);
 			getNode(node, j1 - 1)->next = NULL;
 		}
-		sort(node);
-		sort(pivot);
+		sort(fptr, node);
+		sort(fptr, pivot);
 		if (j1 != 0) {
-			debugCrash(10);
+			debugCrash(fptr, 10);
 			getNode(node, j1 - 1)->next = pivot;
 		}
 	}
