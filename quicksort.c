73b80b2afe97a0ff2f5a9f0dea4167f955f405aa
LuisRizo <personal.luisrizo@gmail.com>
2018-09-26 10:57:51 -0400

    Fixed bugs and clean up

--- quicksort.c
+++ quicksort.c
@@ -7,45 +7,44 @@ Student: Luis Rizo.
 #include <stdlib.h>
 #include <time.h>
 
-struct listnode { struct listnode * next; long key; };
+#define randomindex(k) (rand() % k);
 
+struct listnode { struct listnode * next; long key; };
 void printAddress(struct listnode * p) { printf("%p\n", p); }
 
-int randomindex(int k) { return (rand() % k); }
+struct listnode * getNode(struct listnode * a, int idx);
+struct listnode * sort(struct listnode * a);
 
-void debugCrash(int n) { //printf("Crash Spot #%d", n); 
-}
+long getLengthOfListnode(struct listnode * a);
 
 void swapNodes(struct listnode ** list, int i1, int i2);
-struct listnode * getNode(struct listnode * a, int idx);
-long getLengthOfListnode(struct listnode * a);
-struct listnode * sort(struct listnode * a);
 void printListnode(struct listnode * list);
 
 int main(void)
 {  
-	// char buffer[65536];
-   long i, max_reps = 500, count = 0;
+	const long MAX_ELEMENTS = 500000;
+   long i, elements = MAX_ELEMENTS, count = 0;
    double time_spent;
    clock_t begin, end;
    struct listnode *node, *space;
-	// setvbuf(stdout, buffer, _IOFBF, sizeof(buffer));
 
-   space =  (struct listnode *) malloc(max_reps * sizeof(struct listnode));
+   space =  (struct listnode *) malloc(MAX_ELEMENTS * sizeof(struct listnode));
    while(count < 1) {
+	    elements = MAX_ELEMENTS - count;
 		begin = clock();
-		printf("Begin: %lu", begin);
-   		srand(2012);
-		for( i=0; i< max_reps; i++ )
-		{  (space + i)->key = 2*((17*i)%max_reps);
+		printf("Begin: %lu | Elements: %ld", begin, elements);
+   		srand(time(NULL));
+		for( i=0; i < elements; i++ )
+		{  (space + i)->key = 2*((17*i)%elements);
 			(space + i)->next = (space + (i+1));
 		}
-		(space+max_reps - 1)->next = NULL;
+		(space+elements - 1)->next = NULL;
 		node = space;
+		exit(0);
 		printf("\n prepared list, now starting sort\n");
 		node = sort(node);
 		printf("\n checking sorted list\n");
-		for( i=0; i < max_reps; i++)
+		for( i=0; i < elements; i++)
 		{  if( node == NULL )
 			{  printf("List ended early | %ld\n", i); exit(0);
 			}
@@ -65,7 +64,6 @@ int main(void)
 void printListnode(struct listnode * list) {
 	int i = 0, length = getLengthOfListnode(list);
 	for(i = 0; i < length; i++) {
-		debugCrash(11);
 		printf("%ld,", getNode(list, i)->key);
 	}
 	printf("\n");
@@ -74,15 +72,12 @@ void printListnode(struct listnode * list) {
 struct listnode * getNode(struct listnode * a, int idx) {
 	int i = 0;
 	struct listnode * tmp = a;
-	if (idx < 0) return NULL;
+	if (idx < 0) {
+		return NULL;
+	}
 	while(tmp && i < idx) {
-		// debugCrash(12);
 		tmp = tmp->next;
 		i++;
-	}
-	if (!tmp) {
-		printf("Get node returned a null value when asked for index %d.\n", idx);
-		printListnode(a);
 	}
 	return tmp;
 }
@@ -91,7 +86,6 @@ long getLengthOfListnode(struct listnode * a) {
 	long length = 0;
 	struct listnode * temp = a;
 	while(temp) {
-		// debugCrash(1);
 		temp = temp->next;
 		length++;
 	}
@@ -102,36 +96,29 @@ long getLengthOfListnode(struct listnode * a) {
 void swapNodes(struct listnode ** list, int i1, int i2) {
 	struct listnode * node, * val, * prevNode, * prevVal, * tmp;
 	if (i1 == i2) return;
-	node = getNode(*list, i1);
-	val = getNode(*list, i2);
-	if (!node || !val) return;
-
 	prevNode = getNode(*list, i1 - 1);
 	prevVal = getNode(*list, i2 - 1);
+	node = prevNode->next;
+	val = prevVal->next;
+	if (!node || !val) return;
 
-	if (prevNode){
-		// debugCrash(2);
+	if (prevNode)
 		prevNode->next = val;
-	}else
+	else
 		*list = val;
 
-	if (prevVal){
-		// debugCrash(3);
+	if (prevVal)
 		prevVal->next = node;
-	}else
+	else
 		*list = node;
 
-	debugCrash(4);
 	tmp = val->next;
-	debugCrash(5);
 	val->next = node->next;
-	debugCrash(6);
 	node->next = tmp;
 }
 
 struct listnode * sort(struct listnode * node){
-
-	int i, j1, j2;
+	int i = 0, j1, j2;
 	long length;
 	struct listnode * pivot, * tmp;
 
@@ -139,40 +126,34 @@ struct listnode * sort(struct listnode * node){
 	if (length > 1){
 		i = randomindex(length);
 		pivot = getNode(node, i);
-
 		swapNodes(&node, i, length - 1);
 
 		j1 = 0; j2 = length - 1;
 		tmp = node;
-		while(j1 < j2) {
-			debugCrash(7);
-			while(tmp->key < pivot-> key) {
+		while(tmp && j1 < j2) {
+			while(tmp->key < pivot->key) {
 				j1++;
-				if (tmp->next->key > pivot->key) {
+				if (tmp->next->key >= pivot->key) {
 					break;
 				}
 				tmp = tmp->next;
 			}
-			debugCrash(8);
 			for(; getNode(node, j2)->key >= pivot->key && j2 > j1; j2--);
 			
 			if (j1 != j2) {
 				swapNodes(&node, j1, j2);
-				tmp = tmp->next;
+				tmp = getNode(node, j1); // Update pointer after swap.
 			}
 		}
 		swapNodes(&node, length - 1, j1);
-
-		if (j1 != 0) {
-			debugCrash(9);
-			getNode(node, j1 - 1)->next = NULL;
+		if (j1 == 0) {
+			pivot = node->next;
+			j1++;
 		}
-		sort(node);
-		sort(pivot);
-		if (j1 != 0) {
-			debugCrash(10);
-			getNode(node, j1 - 1)->next = pivot;
-		}
+		getNode(node, j1 - 1)->next = NULL;
+		node = sort(node);
+		pivot = sort(pivot);
+		getNode(node, j1 - 1)->next = pivot;
 	}
 	return node;
 }
